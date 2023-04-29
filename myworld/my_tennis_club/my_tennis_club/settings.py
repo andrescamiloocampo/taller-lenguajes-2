d622acbe93b72455e12efd08214420b039c5738f
Andres Camilo Ocampo <andres_ocampo82211@elpoli.edu.co>
2023-04-29 16:42:24 -0500

    modelos, rutas y plantillas

--- myworld/my_tennis_club/my_tennis_club/settings.py
+++ myworld/my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gkho)ut&!h=m9zq*q^44@9vsd=yot=9@2=xb=6s&*s8y%h=hp6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members'
 ]
 
 MIDDLEWARE = [
